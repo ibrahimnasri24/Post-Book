ba98600e3456cf595b6affdcbbdc8ef693e70480
Ibrahim Nasri <ibrahimnasri24@gmail.com>
2021-09-20 20:09:55 +0300

    setting up a post model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const feedRoutes = require("./routes/feed");
+const feedRoutes = require('./routes/feed');
 
 const app = express();
 
@@ -8,15 +9,24 @@ const app = express();
 app.use(express.json()); // application/json
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.use("/feed", feedRoutes);
+app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    'mongodb+srv://Ibrahim:CeneNJKDhJ8nAt3@cluster0.6geqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
