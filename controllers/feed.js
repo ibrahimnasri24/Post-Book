be73ba284095bc064f2140e12473edfa689efc01
Ibrahim Nasri <ibrahimnasri24@gmail.com>
2021-09-20 21:01:29 +0300

    fetching a single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,19 @@ const { result } = require('lodash');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.jpg',
-        creator: {
-          name: 'Maximilian'
-        },
-        createdAt: new Date()
+  Post.find()
+    .then((posts) => {
+      console.log(posts);
+      res
+        .status(200)
+        .json({ message: 'Fetched posts successfully', posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.status = 500;
       }
-    ]
-  });
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -48,6 +47,25 @@ exports.createPost = (req, res, next) => {
       if (!err.statusCode) {
         err.status = 500;
       }
-      next();
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: 'Post fetched', post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.status = 500;
+      }
+      next(err);
     });
 };
